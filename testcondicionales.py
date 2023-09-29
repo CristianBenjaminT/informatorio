c2a0be6f54c00dc0b7ddad6760b5bb808d656108
unknown <cristiantourn56@gmail.com>
2023-09-29 15:48:41 -0300

    prueba 2

---
+++ testcondicionales.py
@@ -0,0 +1,22 @@
+#if True:
+#   print("cumple condición")
+
+
+
+#veces=0
+#while(veces<10):
+  #print ("no llegué a los 10")
+   # veces= veces+1
+
+
+#tupla= (1,2,3,[4,5,6])
+#print(tupla[3][1])
+
+#promedio=0
+#lista=[6,8,8,8,9]
+#promedio=((lista[0]+lista[1]+lista[2]+lista[3]+lista[4])/5)
+#print(promedio)
+
+numero=input("ingrese un número")
+resultado=numero+ 125
+print (resultado)
